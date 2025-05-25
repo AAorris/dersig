4af47bfb306dced0d1b692c1ea757d28be1e4ee8
Aaron Morris <aaorris@gmail.com>
2025-05-25 03:32:56 -0400

    update dynamic setting

--- src/app/key/page.tsx
+++ src/app/key/page.tsx
@@ -1,6 +1,8 @@
 import { streamKeys } from "./lib";
 import { StreamedKeyList } from "./streamed-key-list";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const { streamableValue, bestPromise } = streamKeys(60_000);
   return <StreamedKeyList streamableValue={streamableValue.value} size={50} />
